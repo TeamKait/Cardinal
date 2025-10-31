e01f31de7e8f0f5654a5a9df962607119f390f8b
ART <teamkait1@gmail.com>
2025-10-31 23:35:46 +0300

    Updates

---
+++ assets/Spinner.vue_vue_type_script_setup_true_lang-hg3YUgZs.js
@@ -0,0 +1,6 @@
+import{ag as c,d as n,c as o,u as a,n as r,g as l,o as t}from"./index-DZzsaz5I.js";/**
+ * @license lucide-vue-next v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=c("loader-circle",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]),m=n({__name:"Spinner",props:{class:{}},setup(s){const e=s;return(p,d)=>(t(),o(a(i),{role:"status","aria-label":"Loading",class:r(a(l)("size-4 animate-spin",e.class))},null,8,["class"]))}});export{m as _};
