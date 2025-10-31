2b20e67f4081c3554e2d595edd0a00b5962e0793
ART <teamkait1@gmail.com>
2025-10-31 22:55:21 +0300

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
         vue(),
         vueDevTools(),
     ],
+    base: "/Cardinal/",
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
